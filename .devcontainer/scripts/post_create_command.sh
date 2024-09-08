24ae09cc8cf502b8cba4b5dfbba989b6e71f1619
Jonathan Kilzi <jkilzi@redhat.com>
2024-09-08 12:31:48 +0000

    chore: Adds openapi-tools-cli
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- .devcontainer/scripts/post_create_command.sh
+++ .devcontainer/scripts/post_create_command.sh
@@ -4,13 +4,15 @@
 # Inside a devcontainer, the container's user (vscode) and the host user both
 # have different UID/GIDs, leading Git to consider the ownership as dubious.
 if [[ -z "$(git config --global safe.directory)" ]]; then
-    git config --global --add safe.directory ${PWD}
+    git config --global --add safe.directory $PWD
 fi
 
 # Replace the default ZSH theme with a custom version the adds a newline before
 # the user's prompt sign 
 cp -f \
-  ${PWD}/.devcontainer/scripts/devcontainers.zsh-theme \
+  $PWD/.devcontainer/scripts/devcontainers.zsh-theme \
   /home/vscode/.oh-my-zsh/custom/themes/devcontainers.zsh-theme
 
 corepack enable && corepack install
+
+echo 'export PATH="$PATH:$PWD/node_modules/.bin"' >> $HOME/.zshrc
