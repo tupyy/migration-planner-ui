2d9d96a28ca075c1d65d79c4373203c400537824
Jonathan Kilzi <jkilzi@redhat.com>
2024-09-25 10:38:34 +0300

    fix: unmock
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- apps/demo/src/main/Root.tsx
+++ apps/demo/src/main/Root.tsx
@@ -6,19 +6,20 @@ import { RouterProvider } from "react-router-dom";
 import { Configuration } from "@migration-planner-ui/api-client/runtime";
 import { SourceApi } from "@migration-planner-ui/api-client/apis";
 import { Spinner } from "@patternfly/react-core";
-import { Container, Provider as DependencyInjectionProvider } from "@migration-planner-ui/ioc";
+import {
+  Container,
+  Provider as DependencyInjectionProvider,
+} from "@migration-planner-ui/ioc";
 import { PLANNER_BASEPATH } from "./Constants";
 import { router } from "./Router";
 import { Symbols } from "./Symbols";
-import { MockSourceApi } from "#/apis/MockSourceApi"; // TODO(jkilzi): REMOVE!!!
 
 function getConfiguredContainer(): Container {
   const plannerApiConfig = new Configuration({
     basePath: PLANNER_BASEPATH,
   });
   const container = new Container();
-  // container.register(Symbols.SourceApi, new SourceApi(plannerApiConfig));
-  container.register(Symbols.SourceApi, new MockSourceApi(plannerApiConfig));
+  container.register(Symbols.SourceApi, new SourceApi(plannerApiConfig));
 
   return container;
 }
