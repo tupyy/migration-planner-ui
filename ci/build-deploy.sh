c66f016233345131bc06e0673e85fdfcfb4a0e72
Nir Argaman <nargaman@redhat.com>
2025-02-12 14:51:44 +1100

    ECOPROJECT-2525: Add openshift templates
    This commit include:
    * Add ui-template yaml, to be used by AppSRE
    * In build-deploy.sh, allow changing MIGRATION_PLANNER_UI_IMAGE before running the script
    
    Signed-off-by: Nir Argaman <nargaman@redhat.com>

--- ci/build-deploy.sh
+++ ci/build-deploy.sh
@@ -4,11 +4,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 cd "$PARENT_DIR"
 
-IMAGE_NAME="quay.io/app-sre/migration-planner-ui"
+export MIGRATION_PLANNER_UI_IMAGE="${MIGRATION_PLANNER_UI_IMAGE:-quay.io/app-sre/migration-planner-ui}"
 GIT_HASH=$(git rev-parse --short=7 HEAD)
 
-podman build . -f Containerfile -t ${IMAGE_NAME}:latest
-podman tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${GIT_HASH}
+podman build . -f Containerfile -t ${MIGRATION_PLANNER_UI_IMAGE}:latest
+podman tag ${MIGRATION_PLANNER_UI_IMAGE}:latest ${MIGRATION_PLANNER_UI_IMAGE}:${GIT_HASH}
 
 DOCKER_CONF="${PWD}/docker-config"
 DOCKER_AUTH_FILE="${DOCKER_CONF}/auth.json"
@@ -16,5 +16,5 @@ mkdir -p "${DOCKER_CONF}"
 
 podman login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io --authfile "${DOCKER_AUTH_FILE}"
 
-podman push "${IMAGE_NAME}:latest" --authfile "${DOCKER_AUTH_FILE}"
-podman push "${IMAGE_NAME}:${GIT_HASH}" --authfile "${DOCKER_AUTH_FILE}"
+podman push "${MIGRATION_PLANNER_UI_IMAGE}:latest" --authfile "${DOCKER_AUTH_FILE}"
+podman push "${MIGRATION_PLANNER_UI_IMAGE}:${GIT_HASH}" --authfile "${DOCKER_AUTH_FILE}"
