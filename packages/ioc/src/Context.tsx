ee4dc05656870764a5200b5f463ad46faeffa680
Jonathan Kilzi <jkilzi@redhat.com>
2026-01-06 22:11:49 +0200

    chore: Update package.json and Provider.tsx for improved structure (#214)
    * chore: Update package.json and Provider.tsx for improved structure
    
    - Removed sideEffects and publishConfig from package.json to streamline configuration.
    - Added "files" entry to package.json to specify distribution files.
    - Updated Provider component type definition for better clarity and consistency.
    
    These changes enhance the package configuration and improve type safety in the Provider component.
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>
    
    * chore: Update biome.json and VSCode settings for improved configuration
    
    - Added "root": true to biome.json to define the project root.
    - Updated file exclusion patterns in biome.json for api-client and agent-client to use relative paths.
    - Introduced overrides in biome.json for specific linter configurations for the ioc package.
    - Set default formatter for various file types in .vscode/settings.json to use Biome, enhancing code formatting consistency.
    
    These changes improve project structure and development experience.
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>
    
    * fix: Update import statements in Context and Provider components to use .js file extensions
    
    - Changed import paths in Context.tsx and Provider.tsx from .ts to .js to ensure compatibility with module resolution.
    - This update aligns the code with the current file structure and improves consistency across the project.
    
    These changes enhance the maintainability and clarity of the codebase.
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>
    
    ---------
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- packages/ioc/src/Context.tsx
+++ packages/ioc/src/Context.tsx
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-import type { Container } from "./Container.ts";
+import type { Container } from "./Container.js";
 
 export const Context = createContext<Container | null>(null);
--- packages/ioc/src/Provider.tsx
+++ packages/ioc/src/Provider.tsx
@@ -1,11 +1,9 @@
-import type React from "react";
-import type { PropsWithChildren } from "react";
 import type { Container } from "./Container.js";
 import { Context } from "./Context.js";
 
-export const Provider: React.FC<PropsWithChildren<{ container: Container }>> = (
-  props,
-) => {
+export const Provider: React.FC<
+  React.PropsWithChildren<{ container: Container }>
+> = (props) => {
   const { container, children } = props;
   return <Context.Provider value={container}>{children}</Context.Provider>;
 };
